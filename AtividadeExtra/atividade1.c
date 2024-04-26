353194e541be58c249e80b7433436a7782deabd4
Szeckir <80286620+Szeckir@users.noreply.github.com>
2024-04-26 08:54:44 -0300

    codigo atualizado

--- AtividadeExtra/atividade1.c
+++ AtividadeExtra/atividade1.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+
+double distanciaEuclidiana(int x1, int y1, int x2, int y2) {
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
 
 void preencheMatriz(int **matriz, int linha, int coluna, int sementes);
 void exibeMatriz(int **matriz, int linha, int coluna);
+void calculaVoronoi(int **matriz, int linha, int coluna, int sementes, int **posicoesSementes);
 
 int main(void) {
     srand(time(NULL));
-    int coluna, linha;
-    int **matriz;
-    int sementes;
+    int coluna, linha, sementes;
+    int **matriz, **posicoesSementes;
 
     printf("Insira as dimensoes da matriz: \n");
     printf("Linhas: ");
@@ -21,10 +26,13 @@ int main(void) {
     scanf("%i", &sementes);
 
     matriz = malloc(linha * sizeof(int *));
-    if (matriz == NULL) {
+    posicoesSementes = malloc(sementes * sizeof(int *));
+    if (matriz == NULL || posicoesSementes == NULL) {
         printf("Erro na alocacao!\n");
         exit(1);
     }
+
+
     for (int i = 0; i < linha; i++) {
         matriz[i] = malloc(coluna * sizeof(int));
         if (matriz[i] == NULL) {
@@ -32,69 +40,87 @@ int main(void) {
             exit(1);
         }
     }
+    for (int i = 0; i < sementes; i++) {
+        posicoesSementes[i] = malloc(2 * sizeof(int));
+        if (posicoesSementes[i] == NULL) {
+            printf("Erro na alocacao!\n");
+            exit(1);
+        }
+    }
 
     preencheMatriz(matriz, linha, coluna, sementes);
     exibeMatriz(matriz, linha, coluna);
-    
+    calculaVoronoi(matriz, linha, coluna, sementes, posicoesSementes);
+    exibeMatriz(matriz, linha, coluna);
+
     for (int i = 0; i < linha; i++) {
         free(matriz[i]);
     }
     free(matriz);
+    for (int i = 0; i < sementes; i++) {
+        free(posicoesSementes[i]);
+    }
+    free(posicoesSementes);
     return 0;
 }
 
 void preencheMatriz(int **matriz, int linha, int coluna, int sementes) {
-    int randomLinha, randomColuna;
-    int *sementesUtilizadas = malloc(sementes * sizeof(int)); 
-    if (sementesUtilizadas == NULL) {
-        printf("Erro na alocacao!\n");
-        exit(1);
-    }
-    for (int i = 0; i < sementes; i++) {
-        sementesUtilizadas[i] = 0;
-    }
     for (int i = 0; i < linha; i++) {
         for (int j = 0; j < coluna; j++) {
-            matriz[i][j] = 0;
+            matriz[i][j] = -1; 
         }
     }
 
     for (int i = 0; i < sementes; i++) {
-        int valorSemente;
-        do {
-            valorSemente = rand() % 50; 
-            int sementeJaUtilizada = 0;
-            for (int k = 0; k < sementes; k++) {
-                if (valorSemente == sementesUtilizadas[k]) {
-                    sementeJaUtilizada = 1;
-                    break;
-                }
-            }
-            if (!sementeJaUtilizada) {
-                sementesUtilizadas[i] = valorSemente;
-                break;
-            }
-        } while (1);
-
+        int randomLinha, randomColuna, valorSemente = i; 
         do {
             randomLinha = rand() % linha;
             randomColuna = rand() % coluna;
-        } while (matriz[randomLinha][randomColuna] != 0);
+        } while (matriz[randomLinha][randomColuna] != -1);
         matriz[randomLinha][randomColuna] = valorSemente;
     }
-    free(sementesUtilizadas);
 }
 
 void exibeMatriz(int **matriz, int linha, int coluna) {
-    printf("\nMatriz com as sementes:\n\n");
+    printf("\nDiagrama de Voronoi:\n\n");
     for (int i = 0; i < linha; i++) {
         for (int j = 0; j < coluna; j++) {
-            if (matriz[i][j] != 0) {
-                printf("%d ", matriz[i][j]);
+            if (matriz[i][j] != -1) {
+                printf("%2d ", matriz[i][j]);
             } else {
-                printf("%c ", '.');
+                printf("%2c ", '.');
             }
         }
         printf("\n");
+    }
+}
+
+void calculaVoronoi(int **matriz, int linha, int coluna, int sementes, int **posicoesSementes) {
+    int semente = 0;
+    for (int i = 0; i < linha && semente < sementes; i++) {
+        for (int j = 0; j < coluna && semente < sementes; j++) {
+            if (matriz[i][j] != -1) {
+                posicoesSementes[semente][0] = i;
+                posicoesSementes[semente][1] = j;
+                semente++;
+            }
+        }
+    }
+
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+            if (matriz[i][j] == -1) { 
+                double menorDistancia = INFINITY;
+                int SementeMaisProxima;
+                for (int k = 0; k < sementes; k++) {
+                    double dist = distanciaEuclidiana(i, j, posicoesSementes[k][0], posicoesSementes[k][1]);
+                    if (dist < menorDistancia) {
+                        menorDistancia = dist;
+                        SementeMaisProxima = matriz[posicoesSementes[k][0]][posicoesSementes[k][1]];
+                    }
+                }
+                matriz[i][j] = SementeMaisProxima;
+            }
+        }
     }
 }
